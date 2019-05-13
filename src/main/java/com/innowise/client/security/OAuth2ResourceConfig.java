63e2f4b610dd6fbf0a515f02e60a43ace936f066
oadmin <mx-store@inbox.ru>
2019-05-13 13:23:06 +0300

    add grant auth_code

--- src/main/java/com/innowise/client/security/OAuth2ResourceConfig.java
+++ src/main/java/com/innowise/client/security/OAuth2ResourceConfig.java
@@ -1,76 +1,76 @@
-package com.innowise.client.security;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
-import org.springframework.security.web.AuthenticationEntryPoint;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.sql.DataSource;
-import java.io.IOException;
-
-@Configuration
-@EnableResourceServer
-public class OAuth2ResourceConfig extends ResourceServerConfigurerAdapter {
-
-    @Value("${security.oauth2.resource.id}")
-    private String resourceId;
-
-
-    @Autowired
-    private DataSource dataSource;
-
-    @Bean
-    public TokenStore tokenStore() {
-        return new JdbcTokenStore(dataSource);
-    }
-
-    @Override
-    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-        resources.resourceId(resourceId).tokenStore(tokenStore());
-    }
-
-
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
-                .and()
-                .authorizeRequests()
-
-                .antMatchers(  "/client/home").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .exceptionHandling()
-                .authenticationEntryPoint(authenticationEntryPoint());
-
-
-    }
-
-
-
-    private AuthenticationEntryPoint authenticationEntryPoint() {
-        return new AuthenticationEntryPoint() {
-            @Override
-            public void commence(HttpServletRequest aRequest, HttpServletResponse aResponse,
-                                 AuthenticationException aAuthException) throws IOException, ServletException {
-                String savedURL = aRequest.getRequestURL().toString();
-                aResponse.sendRedirect("http://localhost:8080/?redirect_uri=" + savedURL);
-
-            }
-        };
-    }
-
-}
+//package com.innowise.client.security;
+//
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.core.AuthenticationException;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+//import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+//import org.springframework.security.oauth2.provider.token.TokenStore;
+//import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+//import org.springframework.security.web.AuthenticationEntryPoint;
+//
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import javax.sql.DataSource;
+//import java.io.IOException;
+//
+//@Configuration
+//@EnableResourceServer
+//public class OAuth2ResourceConfig extends ResourceServerConfigurerAdapter {
+//
+//    @Value("${security.oauth2.resource.id}")
+//    private String resourceId;
+//
+//
+//    @Autowired
+//    private DataSource dataSource;
+//
+//    @Bean
+//    public TokenStore tokenStore() {
+//        return new JdbcTokenStore(dataSource);
+//    }
+//
+//    @Override
+//    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+//        resources.resourceId(resourceId).tokenStore(tokenStore());
+//    }
+//
+//
+//    @Override
+//    public void configure(HttpSecurity http) throws Exception {
+//        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+//                .and()
+//                .authorizeRequests()
+//
+//                .antMatchers(  "/client/home").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .exceptionHandling()
+//                .authenticationEntryPoint(authenticationEntryPoint());
+//
+//
+//    }
+//
+//
+//
+//    private AuthenticationEntryPoint authenticationEntryPoint() {
+//        return new AuthenticationEntryPoint() {
+//            @Override
+//            public void commence(HttpServletRequest aRequest, HttpServletResponse aResponse,
+//                                 AuthenticationException aAuthException) throws IOException, ServletException {
+//                String savedURL = aRequest.getRequestURL().toString();
+//                aResponse.sendRedirect("http://localhost:8080/login?redirect_uri=" + savedURL);
+//
+//            }
+//        };
+//    }
+//
+//}
---
+++ src/main/java/com/innowise/client/security/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.innowise.client.security;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/client/home", "/login**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2Login();;
+    }
+}
