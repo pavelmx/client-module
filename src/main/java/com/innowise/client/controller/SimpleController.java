5f4618d608239a7ca1c3544ffb78a5ffcc25b2e7
oadmin <mx-store@inbox.ru>
2019-06-11 11:21:11 +0300

    change for token oauth2

--- src/main/java/com/innowise/client/controller/SimpleController.java
+++ src/main/java/com/innowise/client/controller/SimpleController.java
@@ -2,24 +2,21 @@ package com.innowise.client.controller;
 
 import com.innowise.client.service.SimpleServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/client")
 @CrossOrigin(origins = "*")
 public class SimpleController {
 
     @Autowired
     SimpleServiceImpl simpleService;
 
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/cars")
     public List<String> getCars(){
         return  simpleService.getList();
@@ -28,7 +25,9 @@ public class SimpleController {
 
     @GetMapping("/home")
     public String getIndex(){
-        return   "Home page resources server";
+        //OAuth2AuthenticationDetails oAuth2AuthenticationDetails = (OAuth2AuthenticationDetails) SecurityContextHolder.getContext().getAuthentication().getDetails();
+        //String token = oAuth2AuthenticationDetails.getTokenValue();
+        return   "Home page resources server token = " ;
     }
 
 }
--- src/main/java/com/innowise/client/security/WebSecurityConfig.java
+++ src/main/java/com/innowise/client/security/WebSecurityConfig.java
@@ -1,22 +1,25 @@
 package com.innowise.client.security;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http.antMatcher("/**")
-                .authorizeRequests()
-                .antMatchers("/", "/client/home", "/login**")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .oauth2Login();;
-    }
+
+
+
 }
